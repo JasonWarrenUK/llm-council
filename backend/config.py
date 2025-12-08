8a2278cd54f09722b6c97c08ea4b1e185a6246ae
Jason Warren <jason@foundersandcoders.com>
2025-12-08 16:44:14 +0000

    feat: add haiku to models

--- backend/config.py
+++ backend/config.py
@@ -5,20 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# KEYS
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # MODELS
 CHATGPT = { "provider": "openai", "model": "gpt-5.1" }
-CLAUDE = { "provider": "anthropic", "model": "claude-sonnet-4-5-20250929" }
 GEMINI = { "provider": "google", "model": "gemini-3-pro-preview" }
+HAIKU = { "provider": "anthropic", "model": "claude-haiku-4-5-20250929" }
+SONNET = { "provider": "anthropic", "model": "claude-sonnet-4-5-20250929" }
 
-CHAIRMAN_MODEL = CLAUDE
-COUNCIL_MODELS = [
-    CHATGPT,
-    # { "provider": "google", "model": "gemini-3-pro-preview" },
-    CLAUDE,
-]
+# ROLES
+CHAIRMAN_MODEL = SONNET
+COUNCIL_MODELS = [ CHATGPT, SONNET ]
 
 DATA_DIR = "data/conversations"
